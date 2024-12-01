e5caba09311546eeba26bc4f9a6e12156ddff71f
Shyama P <shyamap95@gmail.com>
2024-12-01 13:09:18 +0530

    Initial commit

---
+++ Garden Tales/Garden_TalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Garden_TalesApp.swift
+//  Garden Tales
+//
+//  Created by Shyama P on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Garden_TalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Garden TalesTests/Garden_TalesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Garden_TalesTests.swift
+//  Garden TalesTests
+//
+//  Created by Shyama P on 01/12/24.
+//
+
+import Testing
+@testable import Garden_Tales
+
+struct Garden_TalesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
